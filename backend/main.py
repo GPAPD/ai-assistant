70601710f990b80b62f32c1a20962359258ad4b4
akash <akash@AKASH>
2025-05-13 16:18:21 +0530

    adding Ingestion.py

---
+++ backend/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from backend.core import run_llm
+
+# Create FastAPI app
+app = FastAPI()
+
+class Query(BaseModel):
+    message: str
+
+
+# send query to the LLM
+@app.post("/chat")
+def chat(query: Query):
+    user_message = query.message
+    res = run_llm(query=user_message)
+
+    return {
+        "query": res["query"],
+        "response": res["result"],
+        "source_document": res["source_document"]
+    }
